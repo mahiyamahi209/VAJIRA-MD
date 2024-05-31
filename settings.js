6d02f942d73b7d7d5f7b8a5c34a6e33c399d5afd
mahiyamahi209 <mahenbro36@gmail.com>
2024-05-31 10:47:33 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,11 +3,11 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
-global.email = 'vajirarathnayaka891@gmail.com'
+global.email = 'mahenbro372@gmail.com'
 global.github = 'https://github.com/VajirTech/VAJIRA-MD'
 global.location = 'Srilanka Colombo'
-global.packname = '𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢';
-global.author = '𝙑𝘼𝙅𝙄𝙍𝘼 𝙈𝘿';
+global.packname = '𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢-ᵀʰᵉ 𝒮𝓅𝑒𝑒𝒹 𝑅𝒾𝒹𝑒𝓇';
+global.author = '𝓛𝓸𝓻𝓭 𝓜𝓪𝓱𝓲';
 global.mess = {
     banned: '`You are *Banned* fron using commands!`',
     bangc: '`This Group is *Banned* from using Commands!`',
@@ -22,7 +22,7 @@ global.mess = {
     endLimit: '`Your daily limit has expired, the limit will be reset every 12 hours`',
 }
 
-global.owner = [process.env.OWNER_NUMBER] || ['94711453097'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
+global.owner = [process.env.OWNER_NUMBER] || ['94721400708'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
 
 module.exports = {    
 SESSION_ID: process.env.SESSION_ID || 'youre session id',
@@ -30,24 +30,24 @@ POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm
 PREFIX:  process.env.PREFIX || ['.'] ,
 LANG:  process.env.LANG || 'SI' ,    
 ANTI_BAD: process.env.ANTI_BAD || false  ,
-AUTO_REACT:  process.env.AUTO_REACT  || false  ,    
-AUTO_TYPING:  process.env.AUTO_TYPING  || false  ,
-AUTO_RECORDING:  process.env.AUTO_RECORDING  || false  ,
-AUTO_READ:  process.env.AUTO_READ  || false  ,
+AUTO_REACT:  process.env.AUTO_REACT  || true  ,    
+AUTO_TYPING:  process.env.AUTO_TYPING  || true  ,
+AUTO_RECORDING:  process.env.AUTO_RECORDING  || true  ,
+AUTO_READ:  process.env.AUTO_READ  || true  ,
 AUTO_BIO:  process.env.AUTO_BIO  || false  ,    
-ALWAYS_ONLINE:  process.env.ALWAYS_ONLINE  || false  ,            
-WORK_TYPE: process.env.WORK_TYPE || 'private'  ,
-ANTI_LINK: process.env.ANTI_LINK || false  ,
-ANTI_BOT: process.env.ANTI_BOT || false  ,
-OWNER_NUMBER: process.env.OWNER_NUMBER || '94719199757'  ,    
+ALWAYS_ONLINE:  process.env.ALWAYS_ONLINE  || true  ,            
+WORK_TYPE: process.env.WORK_TYPE || 'public'  ,
+ANTI_LINK: process.env.ANTI_LINK || true  ,
+ANTI_BOT: process.env.ANTI_BOT || true  ,
+OWNER_NUMBER: process.env.OWNER_NUMBER || '94721400708'  ,    
 ALIVE: process.env.ALIVE || `default`,
-OWNER_NAME: process.env.OWNER_NAME || 'Vajira' ,     
-AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || false  ,    
-AUTO_VOICE:  process.env.AUTO_VOICE  || false  ,
-AUTO_STICKER: process.env.AUTO_STICKER || false  ,
-WELCOME:  process.env.WELCOME  || false ,    
-NUMBER_212_BLOCK: process.env.NUMBER_212_BLOCK || false  ,
-FOOTER: process.env.FOOTER ||  '©ＶＡＪＩＲＡ - ＭＤ - Ｖ4',
+OWNER_NAME: process.env.OWNER_NAME || '𝓛𝓸𝓻𝓭 𝓜𝓪𝓱𝓲' ,     
+AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || true  ,    
+AUTO_VOICE:  process.env.AUTO_VOICE  || true  ,
+AUTO_STICKER: process.env.AUTO_STICKER || true  ,
+WELCOME:  process.env.WELCOME  || true ,    
+NUMBER_212_BLOCK: process.env.NUMBER_212_BLOCK || true  ,
+FOOTER: process.env.FOOTER ||  '©ᵀʰᵉ 𝒮𝓅𝑒𝑒𝒹 𝑅𝒾𝒹𝑒𝓇 - ＭＤ',
 LOGO: process.env.LOGO || `https://telegra.ph/file/03f1eccdcb525a5e1a6ad.jpg` ,
 IMAGE: process.env.IMAGE || `https://telegra.ph/file/03f1eccdcb525a5e1a6ad.jpg`     
 };
